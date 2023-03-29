e85c56e4b15de75ec4a45c3aaaa86e72abd2b516
Matthieu GROLAUD <matthieu.grolaud@ext.verallia.com>
2023-03-29 18:37:51 +0200

    feat: new

---
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* insert_node - inserts a number into a sorted singly linked list.
+* @head: pointer to the head of the list
+* @number: number to insert
+* Return: the address of the new node, or NULL if it failed
+*/
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new_node;
+	listint_t *current = *head;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = number;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	while (current->n < number)
+	{
+		if (current->next == NULL)
+		{
+			current->next = new_node;
+			return (new_node);
+		}
+
+		if (current->next->n > number)
+		{
+			new_node->next = current->next;
+			current->next = new_node;
+			return (new_node);
+		}
+
+		current = current->next;
+	}
+
+	return (NULL);
+}
---
+++ insert_in_sorted_linked_list/README.md
@@ -0,0 +1,12 @@
+# Insert in sorted linked list
+
+## Requirements
+
+compile with `gcc 4.8.4` using the flags `-Wall -Werror -Wextra and -pedantic`
+`gcc -Wall -Werror -Wextra -pedantic 0-main.c linked_lists.c 0-insert_number.c -o insert`
+
+execute with `./insert`
+
+## Mandatory Tasks
+
+[0. Insert in sorted linked list](./0-insert_number.c)
