7b29dbd3f298e8858cf757feb6c04d5e76e8b293
Matthieu GROLAUD <matthieu.grolaud@ext.verallia.com>
2023-04-28 16:05:32 +0200

    fix: no allocation memory

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -1,17 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 
-int listint_len(listint_t *head)
+/**
+ * is_palindrome_recursive - checks if a singly linked list is a palindrome
+ * 		with recursion
+ * @left: pointer to left of list
+ * @right: pointer to right of list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome_recursive(listint_t *left, listint_t *right)
 {
-	listint_t *current = head;
-	size_t i = 0;
+	if (right->next)
+		is_palindrome_recursive(left, right->next) ? left = left->next : 0;
 
-	while (current != NULL)
-	{
-		current = current->next;
-		i++;
-	}
-	return (i);
+	return (left->n == right->n ? 1 : 0);
 }
 
 
@@ -22,25 +26,8 @@ int listint_len(listint_t *head)
  */
 int is_palindrome(listint_t **head)
 {
-	int i = 0, result = 1;
-	int length = listint_len(*head);
-	listint_t *current = *head;
+	if (*head == NULL)
+		return (1);
 
-	int *array = (int *)malloc(sizeof(int) * length);
-
-	while (current != NULL)
-	{
-		array[i] = current->n;
-		current = current->next;
-		i++;
-	}
-
-	for (i = 0; i < length; i++)
-	{
-		if (array[i] != array[length - 1 - i])
-			result = 0;
-	}
-
-	free(array);
-	return (result);
+	return (is_palindrome_recursive(*head, *head));
 }
