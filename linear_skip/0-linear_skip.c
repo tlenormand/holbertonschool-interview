5e0e2c16111c704154df98854df42c013e3bf98b
tlenormand <lenormand.thomas76110@gmail.com>
2023-07-06 16:11:56 +0200

    fix: betty

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -4,6 +4,13 @@
 #include "search.h"
 
 
+/**
+ * linear_skip - Searches for a value in a sorted skip list of integers
+ * using the jump search algorithm.
+ * @list: Pointer to the head of the skip list to search in.
+ * @value: Value to search for.
+ * Return: Pointer to the first node where value is located or NULL.
+*/
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
 	skiplist_t *current = list, *prev = NULL;
@@ -15,7 +22,8 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 	{
 		prev = current;
 		current = current->express;
-		printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
+		printf("Value checked at index [%lu] = [%d]\n",
+			current->index, current->n);
 
 		if (current->n >= value)
 			break;
@@ -29,11 +37,13 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 			current = current->next;
 	}
 
-	printf("Value found between indexes [%lu] and [%lu]\n", prev->index, current->index);
+	printf("Value found between indexes [%lu] and [%lu]\n",
+		prev->index, current->index);
 
 	while (prev)
 	{
-		printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
+		printf("Value checked at index [%lu] = [%d]\n",
+			prev->index, prev->n);
 
 		if (prev->n == value)
 			return (prev);
