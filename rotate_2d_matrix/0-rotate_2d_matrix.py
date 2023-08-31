69108bcbca7983f7bdd693f96e86f5033f3f3831
tlenormand <lenormand.thomas76110@gmail.com>
2023-08-31 14:01:09 +0200

    feat: new

---
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Rotate 2D Matrix """
+
+def rotate_2d_matrix(matrix):
+    """ Rotate 2D Matrix """
+    matrix.reverse()
+    for i in range(len(matrix)):
+        for j in range(i):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
