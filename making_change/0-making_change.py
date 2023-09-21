1536cbb29e0840878acf6eaae06ee5eade5ea7e3
tlenormand <lenormand.thomas76110@gmail.com>
2023-09-21 14:09:53 +0200

    feat: new

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Change comes from within """
 
+
 def makeChange(coins, total):
     """ Given a pile of coins of different values, determine the fewest number
         of coins needed to meet a given amount total.
