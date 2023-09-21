60fc5b84fc63b0bd8821a3da3ed370c5d36b24ab
tlenormand <lenormand.thomas76110@gmail.com>
2023-09-21 14:06:13 +0200

    feat: new

---
+++ making_change/0-making_change.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" Change comes from within """
+
+def makeChange(coins, total):
+    """ Given a pile of coins of different values, determine the fewest number
+        of coins needed to meet a given amount total.
+
+    Args:
+        coins: (list) list of the values of the coins in your possession
+        total: (int) total amount of change needed
+
+    Returns:
+        (int) fewest number of coins needed to meet total
+    """
+    if total <= 0:
+        return 0
+
+    coins.sort(reverse=True)
+    num_coins = 0
+
+    for coin in coins:
+        if total <= 0:
+            break
+        num_coins += total // coin
+        total = total % coin
+
+    if total != 0:
+        return -1
+
+    return num_coins
