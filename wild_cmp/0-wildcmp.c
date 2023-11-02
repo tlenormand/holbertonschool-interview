d8d2942c4bc9c158bd725d7f3e76212246d244d1
tlenormand <lenormand.thomas76110@gmail.com>
2023-11-02 16:51:38 +0100

    feat: new

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -4,7 +4,8 @@
  * @s2: string 2
  * Return: 1 if the strings can be considered identical, otherwise return 0
  */
-int wildcmp(char *s1, char *s2) {
+int wildcmp(char *s1, char *s2)
+{
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 	if (*s1 == '\0' && *s2 == '*')
