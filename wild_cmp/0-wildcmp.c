ccd21cf604c0dfc27d9ed787cb7c160045e786b8
tlenormand <lenormand.thomas76110@gmail.com>
2023-11-02 16:49:12 +0100

    feat: new

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -1,6 +1,19 @@
-#ifndef WILD_CMP_H
-#define WILD_CMP_H
-
-int wildcmp(char *s1, char *s2);
-
-#endif /* WILD_CMP_H */
+/**
+ * wildcmp - compare two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 1 if the strings can be considered identical, otherwise return 0
+ */
+int wildcmp(char *s1, char *s2) {
+    if (*s1 == '\0' && *s2 == '\0')
+        return (1);
+    if (*s1 == '\0' && *s2 == '*')
+        return (wildcmp(s1, s2 + 1));
+    if (*s1 == '\0' || *s2 == '\0')
+        return (0);
+    if (*s1 == *s2)
+        return (wildcmp(s1 + 1, s2 + 1));
+    if (*s2 == '*')
+        return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+    return (0);
+}
